4edc726c7f98a48cbce5d133ac83854272b4f3f6
wcfreitas <93410040+wcfreitas@users.noreply.github.com>
2023-05-12 13:04:45 -0300

    ensure LoginRouter return a valid token when valid credentials are provided

--- src/presentation/routers/login-router.js
+++ src/presentation/routers/login-router.js
@@ -21,7 +21,7 @@ module.exports = class LoginRouter{
             if(!accessToken){
                 return HttpResponse.unauthorizedError()
             }
-        return HttpResponse.ok()
+        return HttpResponse.ok({accessToken})
         
     }
 }
